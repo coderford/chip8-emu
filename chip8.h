b04201cabc7c0ae1d20ea013db3fc08f421d3be0
Aditya Kumar <kumar.sava.aditya@gmail.com>
2018-07-09 22:34:52 +0530

    Add skeleton initialize and cycle emulation functions

--- chip8.h
+++ chip8.h
@@ -1,4 +1,4 @@
-
+ 
 class chip8 {
 	private:
 		unsigned short opcode;				// stores current opcode
@@ -16,6 +16,27 @@ class chip8 {
 		unsigned short sp;					// stack pointer
 
 		unsigned char key[16];				// hex keypad
-		
 
+	public:
+		void initialize();
+		void emulateCycle();
+}
+
+void chip8::initialize() {
+	// Initialize registers and memory once
+}
+
+void chip8::emulateCycle() {
+	// Fetch opcode
+	opcode = memory[pc] << 8 | memory[pc+1];
+	// ^ we need to read memory twice as each opcode is 2 bytes while the_
+	// memory words are only one byte
+
+	// Decode and execute opcode
+	if((opcode & 0xF000) ^ 0xA000 == 0) { // for opcode 0xANNN, set I to NNN
+		I = opcode & 0x0FFF;
+		pc += 2;
+	}
+
+	// Update timers
 }
