fbbe5a36bf6c23d54955039cc7c29aeef11e60df
Aditya Kumar <kumar.sava.aditya@gmail.com>
2018-07-24 00:35:15 +0530

    Add input capability

--- main.cpp
+++ main.cpp
@@ -31,8 +31,6 @@ int main(int argc, char **argv) {
 		screen[q+3].position = sf::Vector2f(base_x, base_y+SCALE);
 	}
 
-	// setupInput();
-
 	// Initialize the Chip8 system and load the game into the memory
 	myChip8.initialize();
 	myChip8.loadGame(argv[1]);
@@ -41,8 +39,51 @@ int main(int argc, char **argv) {
 	while(window.isOpen()) {
 		sf::Event event;
 		while(window.pollEvent(event)) {
+
 			if(event.type == sf::Event::Closed)
 				window.close();
+
+			// Store key press state (Press and Release)
+			if(event.type == sf::Event::KeyPressed) {
+				switch(event.key.code) {
+					case sf::Keyboard::X:	 myChip8.key[0x0] = 1; break;
+					case sf::Keyboard::Num1: myChip8.key[0x1] = 1; break;
+					case sf::Keyboard::Num2: myChip8.key[0x2] = 1; break;
+					case sf::Keyboard::Num3: myChip8.key[0x3] = 1; break;
+					case sf::Keyboard::Q:	 myChip8.key[0x4] = 1; break;
+					case sf::Keyboard::W:	 myChip8.key[0x5] = 1; break;
+					case sf::Keyboard::E:	 myChip8.key[0x6] = 1; break;
+					case sf::Keyboard::A:	 myChip8.key[0x7] = 1; break;
+					case sf::Keyboard::S:	 myChip8.key[0x8] = 1; break;
+					case sf::Keyboard::D:	 myChip8.key[0x9] = 1; break;
+					case sf::Keyboard::Z:	 myChip8.key[0xA] = 1; break;
+					case sf::Keyboard::C:	 myChip8.key[0xB] = 1; break;
+					case sf::Keyboard::Num4: myChip8.key[0xC] = 1; break;
+					case sf::Keyboard::R:	 myChip8.key[0xD] = 1; break;
+					case sf::Keyboard::F:	 myChip8.key[0xE] = 1; break;
+					case sf::Keyboard::V:	 myChip8.key[0xF] = 1; break;
+				}
+			}
+			if(event.type == sf::Event::KeyReleased) {
+				switch(event.key.code) {
+					case sf::Keyboard::X:	 myChip8.key[0x0] = 0; break;
+					case sf::Keyboard::Num1: myChip8.key[0x1] = 0; break;
+					case sf::Keyboard::Num2: myChip8.key[0x2] = 0; break;
+					case sf::Keyboard::Num3: myChip8.key[0x3] = 0; break;
+					case sf::Keyboard::Q:	 myChip8.key[0x4] = 0; break;
+					case sf::Keyboard::W:	 myChip8.key[0x5] = 0; break;
+					case sf::Keyboard::E:	 myChip8.key[0x6] = 0; break;
+					case sf::Keyboard::A:	 myChip8.key[0x7] = 0; break;
+					case sf::Keyboard::S:	 myChip8.key[0x8] = 0; break;
+					case sf::Keyboard::D:	 myChip8.key[0x9] = 0; break;
+					case sf::Keyboard::Z:	 myChip8.key[0xA] = 0; break;
+					case sf::Keyboard::C:	 myChip8.key[0xB] = 0; break;
+					case sf::Keyboard::Num4: myChip8.key[0xC] = 0; break;
+					case sf::Keyboard::R:	 myChip8.key[0xD] = 0; break;
+					case sf::Keyboard::F:	 myChip8.key[0xE] = 0; break;
+					case sf::Keyboard::V:	 myChip8.key[0xF] = 0; break;
+				}
+			}
 		}
 		// Emulate one cycle
 		myChip8.emulateCycle();
@@ -50,9 +91,6 @@ int main(int argc, char **argv) {
 		// If the draw flag is set, update the screen
 		if(myChip8.drawFlag)
 			drawGraphics(&window);
-
-		// Store key press state (Press and Release)
-		// myChip8.setKeys();
 	}
 
 	return 0;
